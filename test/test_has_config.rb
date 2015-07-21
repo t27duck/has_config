ccacb29c8b326314115c5f937f427c6d6ea4ead6
Tony Drake <t27duck@gmail.com>
2015-07-21 19:45:06 -0400

    Add mass-assignment tests. Label stock tests.

--- test/test_has_config.rb
+++ test/test_has_config.rb
@@ -57,30 +57,32 @@ class TestHasConfig < Minitest::Test
 
     assert object.valid?
 
+    # Test available methods
     default_config.keys.each do |config_key|
       assert object.respond_to?(config_key), "Instance of #{klass} does not respond to #{config_key}"
       assert object.respond_to?("#{config_key}="), "Instance of #{klass} does not respond to #{config_key}="
     end
-
     assert object.respond_to?('enable_email?'), "Instance of #{klass} does not respond to enable_email?"
 
+    # Test setters
     default_config.each do |key, value|
       object.public_send("#{key}=", value)
       assert_equal value, object.public_send(key), "#{key} was not set to #{value}"
     end
 
+    # Test handling of bool values
     ['t', 'true', '1', 1, true].each do |truth_value|
       object.enable_email = truth_value
       assert_equal true, object.enable_email, 'Bool handling did not result in true'
       assert_equal true, object.enable_email?, 'Bool handling did not result in true'
     end
-
     ['f', 'false', '0', 0, false, 'a string', 7].each do |false_value|
       object.enable_email = false_value
       assert_equal false, object.enable_email, 'Bool handling did not result in false'
       assert_equal false, object.enable_email?, 'Bool handling did not result in false'
     end
 
+    # Test nil handling
     object.favorite_color = nil
     assert_equal nil, object.favorite_color, 'nil passed into a string field did not result to nil'
 
@@ -95,5 +97,28 @@ class TestHasConfig < Minitest::Test
 
     object.rate_limit = ''
     assert_equal nil, object.rate_limit, 'Empty string passed into an integer field did not result to a nil'
+
+    # Test mass-assignment
+    object = klass.new(default_config)
+    default_config.each do |key, value|
+      assert_equal value, object.public_send(key), "#{key} was not set to #{value} during mass-assignment"
+    end
+    object.save!
+    object.reload
+    default_config.each do |key, value|
+      assert_equal value, object.public_send(key), "#{key} was not set to #{value} during mass-assignment"
+    end
+
+    # Test setting, saving, and resetting
+    object = klass.new
+    object.favorite_color = 'blue'
+    object.save!
+    object.reload
+    assert_equal 'blue', object.favorite_color
+    object.favorite_color = 'green'
+    object.save!
+    object.reload
+    assert_equal 'green', object.favorite_color
+
   end
 end
