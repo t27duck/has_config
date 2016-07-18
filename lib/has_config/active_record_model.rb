d8bea87e191d2b1ac678afec55cbf00c3b989391
Tony Drake <t27duck@gmail.com>
2016-07-17 20:35:28 -0400

    Move resolved method generation into own method

--- lib/has_config/active_record_model.rb
+++ lib/has_config/active_record_model.rb
@@ -12,7 +12,8 @@ module HasConfig
         raise HasConfig::UnknownConfig, "No setting found for #{key}" if setting.nil?
         raise ArgumentError, "Invalid chain_on option: #{chain_on}" unless CHAINING_OPTIONS.include?(chain_on)
 
-        define_config_getter(setting, parent: parent, chain_on: chain_on)
+        define_config_getter(setting)
+        define_config_resolved(setting, parent: parent, chain_on: chain_on) if parent
         define_config_setter(setting)
         add_config_validations(setting)
       end
@@ -27,7 +28,7 @@ module HasConfig
 
       private ##################################################################
 
-      def define_config_getter(setting, parent: nil, chain_on: nil)
+      def define_config_getter(setting)
         name            = setting.name
         default         = setting.default
         include_boolean = setting.type == :boolean
@@ -37,19 +38,26 @@ module HasConfig
           config[name.to_s].nil? ? default : config[name.to_s]
         end
 
-        define_method("#{name}_resolved") do
-          local_value = public_send(name)
-          if parent && HasConfig::ValueParser.inoke_chain?(local_value, chain_on)
-            public_send(parent).public_send("#{name}_resolved")
-          end
-          return local_value
-        end
-
         if include_boolean
           define_method("#{name}?") do
             config = (attributes[self.class.config_column] || {})
             config[name.to_s].nil? ? default : config[name.to_s]
           end
+        end
+      end
+
+      def define_config_resolved(setting, parent: nil, chain_on: nil)
+        define_method("#{name}_resolved") do
+          local_value = public_send(name)
+          if parent && HasConfig::ValueParser.inoke_chain?(local_value, chain_on)
+            parent_object = public_send(parent)
+            if parent_object.presond_to?("#{name}_resolved".to_sym)
+              parent_object.public_send("#{name}_resolved")
+            else
+              parent_object.public_send(name.to_sym)
+            end
+          end
+          local_value
         end
       end
 
