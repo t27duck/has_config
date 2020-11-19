ef00394f9cbb4e3c64251126d26ece65a0f0d253
Tony Drake <t27duck@gmail.com>
2020-11-19 17:36:36 -0500

    Fix test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,8 +13,8 @@ require 'active_record'
 require 'pg'
 
 db_config = { adapter: 'postgresql', database: 'has_config_test' }
-db_config['username'] = ENV['POSTGRES_USER'] if ENV.key?('POSTGRES_USER')
-db_config['password'] = ENV['POSTGRES_PASSOWRD'] if ENV.key?('POSTGRES_PASSWORD')
-db_config['host'] = ENV['POSTGRES_HOST'] if ENV.key?('POSTGRES_HOST')
+db_config[:username] = ENV['POSTGRES_USER'] if ENV.key?('POSTGRES_USER')
+db_config[:password] = ENV['POSTGRES_PASSOWRD'] if ENV.key?('POSTGRES_PASSWORD')
+db_config[:host] = ENV['POSTGRES_HOST'] if ENV.key?('POSTGRES_HOST')
 
 ActiveRecord::Base.establish_connection(**db_config)
